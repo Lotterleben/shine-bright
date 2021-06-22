972b824fd9785e138c040e8ff578ebe725d3068f
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-06-22 12:19:58 +0200

    use nrf54840-hal in main

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
+use nrf52840_hal as _;
+
 #[entry]
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code

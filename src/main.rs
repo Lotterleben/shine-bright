23a85656ed1b17a9599aff717037f0e7a087ac8c
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-10-09 18:33:56 +0200

    impl blink, improve docs

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,42 @@ use core::fmt::Debug;
 use cortex_m_rt::entry;
 use nrf52840_hal::{
     gpio::{p0::Parts, Level, Output, Pin, PushPull},
+    pac::TIMER0,
     prelude::OutputPin,
+    timer::OneShot,
+    Timer,
 };
 
 use defmt_rtt as _; // global logger
 use panic_halt as _;
+
+const WAIT_CYCLES: u32 = 400_000;
 
 #[derive(Debug)]
 pub enum LedError {
     PeripheralsAlreadyInUse,
 }
 
-// Grants access to the four on-board LEDs of the nrf52840 Development Kit
-pub struct Leds {
-    pub top_left: Led,
-    pub top_right: Led,
-    pub bottom_left: Led,
-    pub bottom_right: Led,
-}
-
+/// Represents one single LED
 pub struct Led {
     pin: Pin<Output<PushPull>>,
+}
+
+/// Represents the nrf52840 Development Kit (constrained to the parts we're actually using)
+pub struct Board {
+    pub top_left_led: Led,
+    pub top_right_led: Led,
+    pub bottom_left_led: Led,
+    pub bottom_right_led: Led,
+
+    /// This is obviously not an LED; but needed for convenient blinking :)
+    pub timer: Timer<TIMER0, OneShot>,
 }
 
 impl Led {
     pub fn on(&mut self) {
         self.pin.set_low().unwrap();
-        //                 ^^^^^^^^ panic on error
+        //                 ^^^^^^^^ note: this panics on error
     }
 
     pub fn off(&mut self) {
@@ -40,29 +49,42 @@ impl Led {
     }
 }
 
-impl Leds {
-    pub fn init() -> Result<Leds, LedError> {
+impl Board {
+    pub fn init() -> Result<Board, LedError> {
         if let Some(periph) = nrf52840_hal::pac::Peripherals::take() {
             let pins = Parts::new(periph.P0);
 
-            let pin_1 = pins.p0_13.degrade().into_push_pull_output(Level::High);
-            //                          ✏️  ^^^^^^^^^  ^^^^^^    ^^^^^^^^^^^^^^^^^^^^  ^^^^^^
-            // get ownership of P0_13 representation     |       configure pin as       High = LED off
-            // (nobody else can have it now!)            |       push-pull output
-            //                                           |
-            //                              degrade Type from "this specific P0_13"
-            //                              to "some Pin" for easier handling
+            let top_left_led_pin = pins.p0_13.degrade().into_push_pull_output(Level::High);
+            //                          ✏️             ^^^^^^^^^  ^^^^^^    ^^^^^^^^^^^^^^^^^^^^  ^^^^^^
+            //           get ownership of P0_13 representation     |       configure pin as       High = LED off
+            //           (nobody else can have it now!)            |       push-pull output
+            //                                                     |
+            //                                        degrade Type from "this specific P0_13"
+            //                                        to "some Pin" for easier handling
 
-            let pin_2 = pins.p0_14.degrade().into_push_pull_output(Level::High);
-            let pin_3 = pins.p0_15.degrade().into_push_pull_output(Level::High);
-            let pin_4 = pins.p0_16.degrade().into_push_pull_output(Level::High);
+            let top_right_led_pin = pins.p0_14.degrade().into_push_pull_output(Level::High);
+            let bottom_left_led_pin = pins.p0_15.degrade().into_push_pull_output(Level::High);
+            let bottom_right_led_pin = pins.p0_16.degrade().into_push_pull_output(Level::High);
 
-            Ok(Self {
-                top_left: Led { pin: pin_1 },
-                top_right: Led { pin: pin_2 },
-                bottom_left: Led { pin: pin_3 },
-                bottom_right: Led { pin: pin_4 },
+            let timer = nrf52840_hal::Timer::new(periph.TIMER0);
+
+            Ok(Board {
+                top_left_led: Led {
+                    pin: top_left_led_pin,
+                },
+                top_right_led: Led {
+                    pin: top_right_led_pin,
+                },
+                bottom_left_led: Led {
+                    pin: bottom_left_led_pin,
+                },
+                bottom_right_led: Led {
+                    pin: bottom_right_led_pin,
+                },
+                timer,
             })
+        //  ^^ note: final statements without a `;` are return statements
+        //     (we're returning our new `Board` here)
         } else {
             Err(LedError::PeripheralsAlreadyInUse)
         }
@@ -74,22 +96,29 @@ fn main() -> ! {
     // logging statement
     defmt::info!("hello leds");
 
-    let mut leds = Leds::init().unwrap();
+    let mut board = Board::init().unwrap();
 
-    let mut my_led = leds.top_right;
+    let mut my_led = board.top_right_led;
     my_led.on();
 
     //✏️ This will lead to a compiler error: `my_led` already owns the `top_right` led
-    //let my_second_led = leds.top_right;
+    //let my_second_led = board.top_right;
 
     // this LED stays on forever:
-    leds.bottom_right.on(); // make it shine
-    drop(leds.bottom_right); // make sure we can't use it anymore after his
+    board.bottom_right_led.on(); // make it shine
+    drop(board.bottom_right_led); // make sure we can't use it anymore after his
 
     // ✏️ This will lead to a compiler error: nobody can take ownership of `bottom_right`,
     // because it has been thrown away forever
-    leds.bottom_right.off();
+    //board.bottom_right.off();
 
-    // make fn divergent (i.e. it never stops)
-    loop {}
+    loop {
+        board.timer.delay(WAIT_CYCLES);
+        my_led.off();
+        defmt::info!("⚪️");
+
+        board.timer.delay(WAIT_CYCLES);
+        my_led.on();
+        defmt::info!("🚨");
+    }
 }
